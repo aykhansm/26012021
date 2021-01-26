1af4b052a3edfbf3e53984ee68cf243d2efd9638
aykhansm <ayxan689@gmail.com>
2021-01-26 15:51:15 +0400

    artirildi

---
+++ 260120211/260120211/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System;
+
+
+
+
+
+namespace ConsoleApp26012020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string str = "Ayxan Ismayilzade burdadirmi?";
+            str.Trim();
+            string str1;
+            int counter = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    counter++;
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine(str);
+                return;
+            }
+            str1 = str.Substring(0, str.IndexOf(' ')) + str.Substring(str.LastIndexOf(' '));
+            Console.WriteLine(str1);
+
+
+            #region 2ci mesele
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
+
